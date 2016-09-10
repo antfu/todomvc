97adf712ab141eb4397d711108e272a32d025f2d
Anthony Fu <anthonyfu117@hotmail.com>
2016-09-10 12:41:47 +0800

    Add tabs

--- static/store.js
+++ static/store.js
@@ -2,17 +2,18 @@
 
 (function (exports) {
 
-	'use strict';
+  'use strict';
 
-	var STORAGE_KEY = 'todos-vuejs';
+  var STORAGE_KEY = 'todos-vuejs-';
 
-	exports.todoStorage = {
-		fetch: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-		},
-		save: function (todos) {
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-		}
-	};
+  exports.todoStorage = {
+    fetch: function (id, defval) {
+      if (defval == undefined) defval = '[]';
+      return JSON.parse(localStorage.getItem(STORAGE_KEY + id) || defval);
+    },
+    save: function (id, todos) {
+      localStorage.setItem(STORAGE_KEY + id, JSON.stringify(todos));
+    }
+  };
 
 })(window);
